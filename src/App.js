bf9579bf81a527e2f4bd0a96ede816de843fbde2
Surendra <surendravikram85@gmail.com>
2024-11-09 22:20:50 +0530

    react- router setup and provide navigation and path

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import About from "./components/About";
+import About from "./components/About";
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
 
-  const [btnText, setBtnText] = useState('Light Mode')
+  const [btnText, setBtnText] = useState("Light Mode");
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      setBtnText('Dark Mode')
+      setBtnText("Dark Mode");
     } else {
       setMode("light");
-      setBtnText('Light Mode')
+      setBtnText("Light Mode");
       document.body.style.backgroundColor = "white";
     }
   };
+
   return (
-    <>
+    <BrowserRouter>
       <Navbar
         title="My First App"
         about="My About"
         mode={mode}
         toggleMode={toggleMode}
-        btnText = {btnText}
+        btnText={btnText}
       />
       <div className="container my-5">
-        <TextForm heading="Enter the text below " mode={mode} />
-        {/* <About mode={mode}/> */}
+        <Routes>
+          <Route
+            path="/"
+            element={<TextForm heading="Enter the text below " mode={mode} />}
+          />
+          <Route path="about" element={<About mode={mode} />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
